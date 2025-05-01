e085b48aaee84a32c8f35e99a76067f444c29047
James Kusmambang <kusmambang@gmail.com>
2025-05-01 16:49:10 +0700

    buat project rust dasar

---
+++ rust-dasar/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn hello_test() {
+    println!("Hello, test!")
+}
+
+#[test]
+fn variable_test() {
+    let name = "Bambang Irwanto";
+    println!("Hello, {}", name);
+}
