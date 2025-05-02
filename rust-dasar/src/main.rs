8ec37a4ad76a93d0d0648f82543226f5183c2638
James Kusmambang <aparalun@gmail.com>
2025-05-02 22:02:47 +0700

    type data number

--- rust-dasar/src/main.rs
+++ rust-dasar/src/main.rs
@@ -12,3 +12,12 @@ fn variable_test() {
     let name = "Bambang Irwanto";
     println!("Hello, {}", name);
 }
+
+#[test]
+fn number_test() {
+    let x: i8 = 10;
+    println!("{}", x);
+
+    let y: f32 = 10.5;
+    println!("{}", y)
+}
